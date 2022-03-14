7479534b4ed2e036c2c8d1805fd65945935cbe93
blanche37 <wkddbsghk16@gmail.com>
2022-03-14 17:28:22 +0900

    [FEAT] EndPointType프로토콜 불필요한 프로퍼티 삭제

--- FLO/NetworkManager/EndPoint.swift
+++ FLO/NetworkManager/EndPoint.swift
@@ -9,10 +9,4 @@ import Foundation
 
 protocol EndPointType {
     var baseURL: URL { get }
-    var path: String { get }
-    var httpMethod: HTTPMethod { get }
-    
-    // 현시점에서 불필요.
-    //    var task: HTTPTask { get }
-    //    var headers: HTTPHeaders? { get }
 }
