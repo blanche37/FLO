29ea3d6cfab1643ef36fd5f0e5b297813e1f72e1
blanche37 <wkddbsghk16@gmail.com>
2022-03-14 17:10:14 +0900

    [FEAT] HTTPMethod 열거형 추가

---
+++ FLO/NetworkManager/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  FLO
+//
+//  Created by yun on 2022/03/14.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
