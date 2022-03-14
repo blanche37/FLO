64b9bd1ba1e1321a5f4e3cd44c8c179518b206e9
blanche37 <wkddbsghk16@gmail.com>
2022-03-14 17:46:38 +0900

    [FEAT] Typealias 삭제 및 Router 클래스 구현

--- FLO/Router/Router.swift
+++ FLO/Router/Router.swift
@@ -7,13 +7,34 @@
 
 import Foundation
 
-typealias NetworkRouterCompletion = (_ data: Data?, _ response: URLResponse?, _ error: Error?) -> ()
-
 protocol NetworkRouter {
-    func request(url: URL, completion: @escaping NetworkRouterCompletion)
+    func request(url: URL, completion: @escaping (Data) -> ())
     func cancel()
 }
 
 class Router: NetworkRouter {
+    private var task: URLSessionTask?
     
+    func request(url: URL, completion: @escaping (Data) -> ()) {
+        let session = URLSession.shared
+        
+        task = session.dataTask(with: url, completionHandler: { data, response, error in
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data,
+                  let response = response as? HTTPURLResponse,
+                  (200..<300).contains(response.statusCode) else {
+                      return
+                  }
+            completion(data)
+        })
+
+        self.task?.resume()
+    }
+    
+    func cancel() {
+        self.task?.cancel()
+    }
 }
