da78af8656b31c4da7970842c5c0854411dd6891
blanche37 <wkddbsghk16@gmail.com>
2022-03-14 17:09:14 +0900

    [FEAT] NetworkRouter 구현

--- FLO/NetworkManager/Router.swift
+++ FLO/NetworkManager/Router.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+typealias NetworkRouterCompletion = (_ data: Data?, _ response: URLResponse?, _ error: Error?) -> ()
+
 protocol NetworkRouter {
+    associatedtype EndPoint: EndPointType
     
+    func request(_ route: EndPoint, completion: @escaping NetworkRouterCompletion)
+    func cancel()
 }
 
 struct Router: NetworkRouter {
