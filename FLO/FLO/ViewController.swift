7e5c829e5ad3b563c44cb41a0395cca79c87fb9a
blanche37 <wkddbsghk16@gmail.com>
2022-03-16 18:59:48 +0900

    [ADD] 각 UI요소 IBOutlet으로 연결

--- FLO/FLO/ViewController.swift
+++ FLO/FLO/ViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var albumLabel: UILabel!
+    @IBOutlet weak var albumImageView: UIImageView!
+    @IBOutlet weak var musicTitleLabel: UILabel!
+    @IBOutlet weak var singerLabel: UILabel!
+    @IBOutlet weak var lyricsLabel: UILabel!
+    @IBOutlet weak var progressSlider: UISlider!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var rewindButton: UIButton!
+    @IBOutlet weak var forwordButton: UIButton!
+    @IBOutlet weak var muteButton: UIButton!
+    @IBOutlet weak var repeatButton: UIButton!
+    @IBOutlet weak var heartButton: UIButton!
+    
     var viewModel: ViewModel!
     
     override func viewDidLoad() {
