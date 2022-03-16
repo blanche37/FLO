0c7bce3dfa68a6276c5c4beea270d26109415874
blanche37 <wkddbsghk16@gmail.com>
2022-03-17 01:07:23 +0900

    [FEAT] Observable 타입 구현

---
+++ FLO/FLO/Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Observable.swift
+//  FLO
+//
+//  Created by yun on 2022/03/17.
+//
+
+import Foundation
+
+class Observable<T> {
+    var listener: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: ((T) -> Void)?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
